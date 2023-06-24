358a613b607a3399e70fe466b2b388cfaeed2fe8
iampsrv <iampsrv@gmail.com>
2023-06-24 13:40:44 +0530

    second commit

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "web" {
   ami                         = var.ami_id
   instance_type               = var.instance_type
   subnet_id                   = aws_subnet.subnet1.id
-  security_groups             = ["sg-06d632dce2ab48177"]
+  security_groups             = [aws_security_group.mysg.id]
   associate_public_ip_address = true
   user_data                   = file("start.sh")
   tags = {
@@ -15,7 +15,7 @@ resource "aws_instance" "web1" {
   instance_type               = var.instance_type
   subnet_id                   = aws_subnet.subnet2.id
   associate_public_ip_address = true
-  security_groups             = ["sg-06d632dce2ab48177"]
+  security_groups             = [aws_security_group.mysg.id]
   user_data                   = file("start.sh")
   tags = {
     Name = "test-ec2"
@@ -26,7 +26,7 @@ resource "aws_instance" "web2" {
   ami                         = var.ami_id
   instance_type               = var.instance_type
   subnet_id                   = aws_subnet.subnet3.id
-  security_groups             = ["sg-06d632dce2ab48177"]
+  security_groups             = [aws_security_group.mysg.id]
   associate_public_ip_address = true
   user_data                   = file("start.sh")
   tags = {
